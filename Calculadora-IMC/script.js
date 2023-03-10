057973d9bd59948e5213a9a00e44af3e5f1c12ad
Arthur Gabriel <arthurgdasilva77@gmail.com>
2023-03-09 21:13:37 -0300

    Update script.js

--- Calculadora-IMC/script.js
+++ Calculadora-IMC/script.js
@@ -1,39 +1,18 @@
 const button = document.querySelector('#button')
 
-function imc(){
 
+const height = document.querySelector('#altura').value
+const weight = document.querySelector('#peso').value
+const result = document.querySelector('.resultado')
+
+let levels = [
+    {classifications: `, Você está abaixo do Peso ideal`, imc: [0, 18.5]}
+]
+
+const calculateIMC = () => {
     const name = document.querySelector('#nome').value
-    const height = document.querySelector('#altura').value
-    const weight = document.querySelector('#peso').value
-    const result = document.querySelector('.resultado')
-
-    
-    if(name !== '' && height !== '' && weight !== ''){
-
-        const imcValue = (weight / (height * height)).slice(3)
-        let classification = ''
-        
-
-        if(imcValue < 18.5){
-            classification = 'abaixo do peso'
-        } else if (imcValue < 25){
-            classification = 'com o peso ideal. Parabéns!!!'
-        } else if (imcValue < 30){
-            classification =  'levemente acima do peso'
-        } else if (imcValue < 35){
-            classification = 'com obesidade grau I'
-        } else if (imcValue < 40){
-            classification = 'com obesidade grau II'
-        } else {
-            classification = 'com obesidade grau III'
-        }
-
-        result.innerHTML = `Olá ${name}, seu IMC é de ${imcValue} e você está ${classification}`
-    } else {
-        result.innerHTML = 'Preencha todos os campos!!!'
-    }
-
-
+    console.log(name)
 }
 
-button.addEventListener('click', imc)
+
+button.addEventListener('click', calculateIMC)
