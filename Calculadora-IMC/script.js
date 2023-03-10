bf069c7d22ed0e56499d74215f495ff497867429
Arthur Gabriel <arthurgdasilva77@gmail.com>
2023-03-09 21:52:58 -0300

    improving javascript code

--- Calculadora-IMC/script.js
+++ Calculadora-IMC/script.js
@@ -1,18 +1,40 @@
 const button = document.querySelector('#button')
 
-
-const height = document.querySelector('#altura').value
-const weight = document.querySelector('#peso').value
-const result = document.querySelector('.resultado')
-
 let levels = [
-    {classifications: `, Você está abaixo do Peso ideal`, imc: [0, 18.5]}
+    {classification: `, Você está abaixo do Peso ideal`, imc: [0, 18.5]},
+    {classification: ', Você está no peso ideal, Parabéns !!!', imc: [18.6, 24.9]},
+    {classification: ', Você está acima do peso ideal', imc: [25, 30]},
+    {classification: ', Você está com obesidade, cuidado!!!', imc: [30.1, 99999999]}
 ]
 
-const calculateIMC = () => {
+const calculateIMC = (height, weight) => {
+    const result = document.querySelector('.resultado')
     const name = document.querySelector('#nome').value
-    console.log(name)
+    
+    const imc = weight / (height * height)
+
+    for(let i in levels){
+        if(imc >= levels[i].imc[0] && imc < levels[i].imc[1]){
+            console.log(levels[i])
+            result.innerHTML = `${name}${levels[i].classification} e seu imc é ${imc.toFixed(1)}`
+        }
+    }
 }
 
+const executeCalc = () => {
+    let height = document.querySelector('#altura').value
+    let weight = document.querySelector('#peso').value
+    let name = document.querySelector('#nome').value
 
-button.addEventListener('click', calculateIMC)
+    if(height === '' || weight === '' || name === ''){
+        alert('preencha todos os campos')
+    } else{
+        calculateIMC(height, weight)
+
+        document.querySelector('#altura').value = ''
+        document.querySelector('#peso').value = ''
+        document.querySelector('#nome').value = ''
+    }
+}
+
+button.addEventListener('click', executeCalc)
